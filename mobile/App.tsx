1d0d7bb876a51d8b12aebaa1fd177372f2e24cd7
DanielTo1231 <e2155899@cmaisonneuve.qc.ca>
2025-02-02 05:11:23 -0500

    refactor(App): simplify App component structure and remove unused imports and fix visuel issue

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,10 +2,6 @@ import React, {useState} from 'react';
 import { View, Text } from 'react-native';
 import tw from 'twrnc';  // Import twrnc
 import AppNavigator from './navigation/AppNavigator';
-import SplashScreen from './screens/SplashScreen';
-import Home from './screens/Home';
-import PieChartComponent from './components/PieChart';
-import InterestGraphComponent from './components/InterestGraphComponent';
 
 const chartData = [
   { name: 'Stocks', population: 40 },
@@ -21,9 +17,8 @@ export default function App() {
   const [currentScreen, setCurrentScreen] = useState<'Splash' | 'Home'>('Splash');
 
   return (
-    <View style={tw`flex-1 justify-center items-center bg-gray-100`}>
-        return <AppNavigator />;
-
+    <View style={tw`flex-1`}>
+        <AppNavigator />;
     </View>
   );
 }
