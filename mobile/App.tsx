319ef4d6ae87f930cf72b572391ecdfcd38e405d
DanielTo1231 <e2155899@cmaisonneuve.qc.ca>
2025-02-02 09:41:03 -0500

    Refactor navigation structure to use AppNavigator and include FinancialAdviceScreen

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,26 +1,11 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import QuestionnaireSlideshow from '../mobile/screens/QuestionnaireScreen';
-import FinancialAdviceScreen from '../mobile/screens/FinancialAdviceScreen';
-
-const Stack = createStackNavigator();
+import { View } from 'react-native';
+import AppNavigator from './navigation/AppNavigator';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="QuestionnaireSlideshow">
-        <Stack.Screen
-          name="QuestionnaireSlideshow"
-          component={QuestionnaireSlideshow}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="FinancialAdviceScreen"
-          component={FinancialAdviceScreen}
-          options={{ title: 'Financial Advice' }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={{ flex: 1 }}>
+      <AppNavigator />
+    </View>
   );
 }
