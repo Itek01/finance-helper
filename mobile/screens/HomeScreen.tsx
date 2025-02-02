bae9fe4bea3956fdeb0a43af06f8176edeca781e
Victoria Doan <17170708@reginaassumpta.qc.ca>
2025-02-01 20:31:46 -0500

    questionnaire ui

--- mobile/screens/HomeScreen.tsx
+++ mobile/screens/HomeScreen.tsx
@@ -5,7 +5,7 @@ import tw from 'twrnc';  // Import twrnc
 export default function HomeScreen({ navigation }: any) {
   return (
     <View style={tw`flex-1 justify-center items-center bg-white`}>
-      <Text style={tw`text-2xl font-bold mb-4`}>Home Screheyyyyen</Text>
+      <Text style={tw`text-2xl font-bold mb-4`}>Home Screheyhyhhhhhhhhhhhhhhhhhhyyyen</Text>
       <Button
         title="Go to Details"
         onPress={() => navigation.navigate('Details')}
