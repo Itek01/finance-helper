c5e499478322b060a212e72912e48363983084d2
DanielTo1231 <e2155899@cmaisonneuve.qc.ca>
2025-02-02 05:43:18 -0500

    refactor(AppNavigator): remove Login and Signup screens from navigation

--- mobile/navigation/AppNavigator.tsx
+++ mobile/navigation/AppNavigator.tsx
@@ -3,8 +3,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SplashScreen from '../screens/SplashScreen';
 import CoverPage from '../screens/CoverPage';
-import Login from '../screens/Login';
-import Signup from '../screens/Signup';
 import Home from '../screens/Home';
 import QuestionnaireSlideshow from '../screens/QuestionnaireScreen';
 
@@ -16,8 +14,6 @@ const AppNavigator = () => {
       <Stack.Navigator initialRouteName="SplashScreen">
         <Stack.Screen name="SplashScreen" component={SplashScreen} />
         <Stack.Screen name="CoverPage" component={CoverPage} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Signup" component={Signup} />
         <Stack.Screen name="QuestionnaireScreen" component={QuestionnaireSlideshow} />
         <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
