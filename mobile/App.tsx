2edcbce20e465f3d0782145e550bdc78fd4d0dca
DanielTo1231 <e2155899@cmaisonneuve.qc.ca>
2025-02-02 05:12:20 -0500

    refactor(App): remove unused chartData array from App component

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,16 +3,6 @@ import { View, Text } from 'react-native';
 import tw from 'twrnc';  // Import twrnc
 import AppNavigator from './navigation/AppNavigator';
 
-const chartData = [
-  { name: 'Stocks', population: 40 },
-  { name: 'Bonds', population: 20 },
-  { name: 'Crypto', population: 15 },
-  { name: 'Real Estate', population: 25 },
-  { name: 'bee Estate', population: 10 },
-  { name: 'cool Estate', population: 5 },
-
-];
-
 export default function App() {
   const [currentScreen, setCurrentScreen] = useState<'Splash' | 'Home'>('Splash');
 
